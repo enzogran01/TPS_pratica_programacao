b742ed0219abbe4c11d658975e65d8c12cd53918
unisanta <unisanta@santacecilia.br>
2026-08-05 19:51:38 -0300

    first commit

---
+++ PrimeiroExercicio/Prova/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    class Cliente
+    {
+        private static String cnpj;
+        private static String nome;
+
+        public void setCNPJ(String _cnpj) { cnpj = _cnpj; }
+        public void setNome(String _nome) { nome = _nome; }
+        public String getCNPJ() { return cnpj; }
+        public String getNome() { return nome; }
+    }
+}
