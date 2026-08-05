25c22b142a925017ef55aeca3168e143c236f423
unisanta <unisanta@santacecilia.br>
2026-08-05 20:40:24 -0300

    backup01

---
+++ SegundoExercicio/Prova/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BLL.conecta();
+            if (Erro.getErro())
+                MessageBox.Show(Erro.getMsg());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BLL.desconecta();
+        }
+
+    }
+}
