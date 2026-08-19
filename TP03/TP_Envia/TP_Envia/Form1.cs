06671f1af87ca3fca816125a27e160c572683652
unisanta <unisanta@santacecilia.br>
2026-08-19 20:49:07 -0300

    se segurando :sparkles:

---
+++ TP03/TP_Envia/TP_Envia/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TP_Envia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket socketenviar = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            IPEndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+
+            socketenviar.SendTo(Encoding.ASCII.GetBytes(textBox1.Text), endereco);
+            socketenviar.Close();
+        }
+    }
+}
---
+++ TP03/TP_Recebe/TP_Recebe/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TP_Recebe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket socketreceber = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            EndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+            byte[] data = new byte[1024];
+            int qtdbytes;
+
+            socketreceber.Bind(endereco);
+            qtdbytes = socketreceber.ReceiveFrom(data, ref endereco);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data, 0, qtdbytes));
+        }
+    }
+}
