0496d3db36fc6df4af128451cd08d6b0456e04bc
unisanta <unisanta@santacecilia.br>
2026-08-19 21:17:49 -0300

    :recycle: gira

--- TP03/TP_Recebe/TP_Recebe/Form1.cs
+++ TP03/TP_Recebe/TP_Recebe/Form1.cs
@@ -21,18 +21,22 @@ namespace TP_Recebe
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             Socket socketreceber = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
             EndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
             byte[] data = new byte[1024];
             int qtdbytes;
-            button1.Enabled = false;
+
+            socketreceber.Bind(endereco);
 
             while (true)
             {
-                socketreceber.Bind(endereco);
                 qtdbytes = socketreceber.ReceiveFrom(data, ref endereco);
                 listBox1.Items.Add(Encoding.ASCII.GetString(data, 0, qtdbytes));
+                Refresh();
             }
+            socketreceber.Close();
+
         }
     }
 }
