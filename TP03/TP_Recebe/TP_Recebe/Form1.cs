8c5bdc336f7895878ec85a6eda890cf4897cfad7
unisanta <unisanta@santacecilia.br>
2026-08-19 22:06:12 -0300

    coco

--- TP03/TP_Recebe/TP_Recebe/Form1.cs
+++ TP03/TP_Recebe/TP_Recebe/Form1.cs
@@ -9,31 +9,40 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace TP_Recebe
 {
     public partial class Form1 : Form
     {
+        Thread minhaThread;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {            
+            minhaThread = new Thread(new ThreadStart(this.processo));
+            minhaThread.Start();
+        }
+
+        private void processo()
         {
-            
             Socket socketreceber = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
             EndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
             byte[] data = new byte[1024];
-            int qtdbytes;
-
             socketreceber.Bind(endereco);
+            int qtdbytes;
 
             while (true)
             {
                 qtdbytes = socketreceber.ReceiveFrom(data, ref endereco);
-                listBox1.Items.Add(Encoding.ASCII.GetString(data, 0, qtdbytes));
-                Refresh();
+                listBox1.Invoke((Action)delegate ()
+                {
+                    listBox1.Items.Add(Encoding.ASCII.GetString(data, 0, qtdbytes));
+                });
+                
             }
             socketreceber.Close();
 
